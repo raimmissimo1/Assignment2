1032167010ca126a011766e492145d0410e35fec
User <rohakassimbekov@gmail.com>
2025-12-22 17:16:45 +0500

    employee class mostly finished

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,4 +1,67 @@
 package model;
 
 public class Employee implements Person{
+
+    private static int nextId = 1;
+
+    private int id;
+    private String name;
+    private String surname;
+    private String position;
+    private double salary;
+
+    public Employee(){
+        this.id = nextId++;
+    }
+
+    public Employee(String name , String surname , String position , double salary){
+        this.id = nextId++;
+        setName(name);
+        setSurname(surname);
+        setPosition(position);
+        setSalary(salary);
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public String getPosition(){
+        return position;
+    }
+
+    public void setPosition(String position){
+        this.position = position;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString(){
+        return id + " " + name + " " + surname + " " + position + " " + salary;
+    }
 }
+
