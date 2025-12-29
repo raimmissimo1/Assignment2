026aa8508ad0c7920ebb2e567fe14bf3fcb7e041
User <rohakassimbekov@gmail.com>
2025-12-29 10:09:50 +0500

    ...

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,6 +1,6 @@
 package model;
 
-public Person implements Payable , Comparable<Person> {
+public abstract Person implements Payable , Comparable<Person> {
 
     private static int genId = 1;
 
@@ -10,39 +10,53 @@ public Person implements Payable , Comparable<Person> {
 
     public Person(){
         this.id = genId++;
+
     }
 
     public Person(String name , String surname){
         this();
         setName(name);
         setSurname(surname);
+
     }
 
     public int getId(){
         return id;
+
     }
 
     public String getName(){
         return name;
+
     }
 
     public void setName(String name){
         this.name = name;
+
     }
 
     public String getSurname(){
-        return surname;;
+        return surname;
+
     }
 
     public void setSurname(String surname){
         this.surname = surname;
+
     }
 
     public abstract String getPosition();
 
     @Override
+    public int compareTo(Person other){
+        return Double.compare(this.getPaymentAmount() , other.getPaymentAmount());
+
+    }
+
+    @Override
     public String toString(){
         return id + " " + name + " " + surname;
+
     }
 
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -2,59 +2,51 @@ package model;
 
 public class Student extends Person {
 
-    private static int genId = 1;
-
-    private int id;
-    private String name;
-    private String surname;
     private double gpa;
 
     public Student(){
-        this.id = genId++;
+        super();
+
     }
 
     public Student(String name , String surname , double gpa){
         this();
-        setName(name);
-        setSurname(surname);
+        super(name , surname);
         setGpa(gpa);
-    }
 
-    public int getId(){
-        return id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getSurname(){
-        return surname;
-    }
-
-    public void setSurname(String surname){
-        this.surname = surname;
     }
 
     public double getGpa(){
         return gpa;
+
     }
 
     public void setGpa(double gpa){
         this.gpa = gpa;
+
+    }
+
+    @Override
+    public String getPosition(){
+        return "Student";
+
     }
 
     @Override
     public int compareTo(Person other){
-        return double.compare(this.getPaymentAmount() , other.getPaymentAmount());
+        if(gpa > 2.67){
+            return 36660.0;
+
+        }
+        return 0.0;
+
     }
+
+
 
     @Override
     public String toString(){
-        return id + " " + name + " " + surname;
+        return "Student " + super.toString();
+
     }
 }
